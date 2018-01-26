11c1c4be4d3ae9de45aa568f386bda876b14de32
Jakob Lanstorp <jakob@lanstorp.com>
2018-01-26 13:32:01 +0100

    Update adm_pcjupiterxl.sql

--- adm_pcjupiterxl.sql
+++ adm_pcjupiterxl.sql
@@ -9,7 +9,7 @@
   -PostGIS extension for spatial data
   -Tablefunc extension for pivot tables
   -Function for converting to lower case column names
-  -Role and user grants
+  -Role creation and user grants
    
    begin                : 2017-03-14
    copyright            : (C) 2017 EPA
@@ -28,16 +28,46 @@
 /*
   Restore pcjupiterxl from GEUS dump
 */
---  "C:\PostgreSQL\pg10\bin\pg_restore" --host=localhost --port=5432 --username="postgres" --password -c -d pcjupiterxl "C:\data\jupiter\pcjupiterxlplus20171206_full_xl\pcjupiterxlplus20171206_full_xl.backup"
+-- Try to resore data to a schema named jupiter:
+-- CREATE SCHEMA jupiter;
+-- SET search_path TO myschema;
+
+
+-- Restore backup:
+--  "C:\PostgreSQL\10\bin\pg_restore" --host=localhost --port=5432 --username="postgres" --password -c -d pcjupiterxl "D:\jupiter\pcjupiterxlplus_full_xl_2018jan24\pcjupiterxlplus_full_xl.backup"
+-- Test age of database: SELECT "SAMPLEDATE", "INSERTDATE" from public.grwchemsample ORDER BY "INSERTDATE" DESC;
+-- Backup restore version from 2018-01-22 had "WARNING: error ignored on restore: 520"
+
+
+/*
+  If database wrongly ended up in public schema.
+  Do not move PostGIS system tables from public schema.
+*/
+DO
+$$
+DECLARE
+    row record;
+BEGIN
+    FOR row IN SELECT tablename FROM pg_tables WHERE schemaname = 'public' -- and other conditions, if needed
+    LOOP
+        IF row.tablename = ANY( ARRAY['layer_styles', 'spatial_ref_sys']) THEN
+          raise notice 'Don''t move PostGIS system tabels: %', row.tablename;
+        ELSE
+          raise notice 'Moving table: %', row.tablename;
+          EXECUTE 'ALTER TABLE public.' || quote_ident(row.tablename) || ' SET SCHEMA jupiter;';
+        END IF;
+    END LOOP;
+END;
+$$;
 
 /*
   Extensions - Install and create the extensions PostGIS and tablefunc (pivot).
 
 	Extensions information:	
 	SELECT pg_available_extensions();   	--shows extensions available for installation
-	SELECT * from pg_extension;		--show installed extensions
+	SELECT * from pg_extension;				--show installed extensions
 	
-	Test postgis: SELECT postgis_version();	--returns version of postgis
+	Test postgis: SELECT postgis_version();				          --returns version of postgis
 	Test tablefunc: SELECT * FROM normal_rand(1000, 5, 3);	--returns 1000 double precision rows
 */
 CREATE EXTENSION postgis;
@@ -45,7 +75,7 @@ CREATE EXTENSION postgis;
 CREATE EXTENSION tablefunc;
 
 /*
-  Make lower letters of column names of all jupiter tables in schema public.
+  Make lower letters of column names of all jupiter tables in schema jupiter.
   Change WHERE c.table_schema = 'public' for other schema use
   Rewritten from: http://www.postgresonline.com/journal/archives/141-Lowercasing-table-and-column-names.html	
 */
@@ -57,7 +87,7 @@ BEGIN
   FOR rec IN
     SELECT (c.table_schema), c.table_name, c.column_name
       FROM information_schema.columns As c
-      WHERE c.table_schema = 'public'
+      WHERE c.table_schema = 'jupiter'
           AND c.column_name <> lower(c.column_name)
       ORDER BY c.table_schema, c.table_name, c.column_name
     LOOP
@@ -66,6 +96,22 @@ BEGIN
    END LOOP;
 END
 $$;
+
+/*
+	User grants for reader
+	DELETE Role used instead ...
+*/
+--REVOKE ALL ON ALL FUNCTIONS IN SCHEMA public FROM jupiterreader;
+--REVOKE ALL ON ALL TABLES IN SCHEMA public FROM jupiterreader;
+--REVOKE USAGE ON SCHEMA public FROM jupiterreader;
+--DROP USER IF EXISTS jupiterreader;
+--CREATE USER jupiterreader WITH PASSWORD 'mst';                --login default for user not for role creation
+--GRANT USAGE ON SCHEMA public TO jupiterreader;
+--GRANT SELECT ON ALL TABLES IN SCHEMA public TO jupiterreader;
+--GRANT SELECT ON TABLE public.layer_styles TO jupiterreader;  -- TODO without qgis processing hangs for ever - report it
+--GRANT SELECT ON TABLE public.spatial_ref_sys TO jupiterreader;
+--GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO jupiterreader;
+
 
 /*
   Jupiter reader role used by all users but postgres
@@ -79,9 +125,9 @@ GRANT USAGE ON SCHEMA public TO jupiterrole;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO jupiterrole;
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO jupiterrole;
 --GRANT SELECT ON TABLE public.layer_styles TO jupiterole;      -- if jupiter not in public schema
---GRANT SELECT ON TABLE public.spatial_ref_sys TO jupiterrole;  -- if jupiter not in public schema 
+--GRANT SELECT ON TABLE public.spatial_ref_sys TO jupiterrole;  -- if jupiter not in public schema
 
 --Jakob Lanstorp, jalan
 DROP USER IF EXISTS jupiter_jalan;
-CREATE USER jupiter_jalan WITH LOGIN PASSWORD '¤%&#¤%&%&&#¤%/&(/&(/&(#&#¤&#&¤%//&(/&(/()%(¤&/#¤/&#';
+CREATE USER jupiter_jalan WITH LOGIN PASSWORD 'setyourpasswordhere';
 GRANT jupiterrole TO jupiter_jalan;
